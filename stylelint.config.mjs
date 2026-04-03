77b3432f7ac1a278f83d524a893f4b8ed5a802ba
Prince <hipogrifo11@gmail.com>
2026-04-03 15:07:35 +0200

    Styleint and some fixes
    + Installed Styleint
    + Fixed external links to open in new tab
    + Other minor fixes

---
+++ stylelint.config.mjs
@@ -0,0 +1,11 @@
+/** @type {import("stylelint").Config} */
+export default {
+  "extends": ["stylelint-config-standard"],
+  "rules": {
+    "declaration-property-unit-allowed-list": {
+      "/^border/": ["px"],
+      "/^padding|^gap/": ["rem"]
+    },
+   "unit-allowed-list": ["%", "deg", "px", "rem", "ms"]
+  }
+};
