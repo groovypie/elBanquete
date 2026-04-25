32936d89d73229a3a724238cd781ae7fe0e15225
Prince <hipogrifo11@gmail.com>
2026-04-25 21:28:32 +0200

    Animations + responsive images
    Played a little with transitions and .svg animations. Aplied sharp (image resize) and art direction to change images according to dispositive.

--- stylelint.config.mjs
+++ stylelint.config.mjs
@@ -3,8 +3,8 @@ export default {
   "extends": ["stylelint-config-standard"],
   "rules": {
     "declaration-property-unit-allowed-list": {
-      "/^border/": ["px"],
-      "/^padding|^gap/": ["rem"]
+      "/^border/": ["px", "rem"],
+      "/^padding|^gap/": ["px", "rem"]
     },
    "unit-allowed-list": ["%", "deg", "px", "rem", "ms"]
   }
